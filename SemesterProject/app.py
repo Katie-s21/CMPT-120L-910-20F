cbce30a55fb6efd50af836d487cb6a8a6376cefe
Katie-s21 <70357895+Katie-s21@users.noreply.github.com>
2020-11-09 14:17:04 -0500

    Add files via upload

--- SemesterProject/app.py
+++ SemesterProject/app.py
@@ -1,6 +1,20 @@
 from flask import Flask
+from flask import render_template
+
 app = Flask(__name__)
 
 @app.route('/')
+def index():
+    return render_template("hello.html")
+
+import emoji
+
+@app.route('/hello/')
 def hello_world():
-    return 'Hello, World!'
+    return emoji.emojize('Hello me! :smiley:' , use_aliases=True)
+
+
+
+
+
+
