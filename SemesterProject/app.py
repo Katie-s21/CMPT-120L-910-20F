2ff0632b1ba7264da40b2aac292fbe1efa6ba368
Katie-s21 <70357895+Katie-s21@users.noreply.github.com>
2020-11-22 00:04:56 -0500

    Assignment 9

--- SemesterProject/app.py
+++ SemesterProject/app.py
@@ -1,5 +1,8 @@
 from flask import Flask
 from flask import render_template
+import logging
+
+logging.basicConfig(filename='app.log',level=logging.DEBUG)
 
 app = Flask(__name__)
 
@@ -8,12 +11,18 @@ def index():
     return render_template("hello.html")
 
 
-
 @app.route('/hello/')
 def hello_world():
+    logging.info('Welcome to page 2!')
     return ('Hello Me!')
+    
 
-Hello = Hello
+@app.route('/hiya/')
+def hiya_world():
+    logging.info('Going to page 3!')
+    return ('Hiya everyone!')
+
+
 
 
 
